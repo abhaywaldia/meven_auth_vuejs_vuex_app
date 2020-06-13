034b74440c40fa1c25461994a6a60368eaf0ea56
abhaywaldia <abhaysinghwaldia9@gmail.com>
2020-06-13 17:17:02 +0530

    Update keys.js
    Chnage config file to include production key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURL: "mongodb://localhost:27017/meven_auth",
-    secret: 'yoursecret'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURL: "mongodb+srv://abby77:J@uravMDB77@cluster0-tdlkf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    }
+}else{
+    module.exports = {
+        mongoURL: "mongodb://localhost:27017/meven_auth",
+        secret: 'yoursecret'
+    }
 }
